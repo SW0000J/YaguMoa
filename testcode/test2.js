6569e5ae6cb1390811a291082b1b92e6ebf5b341
SW0000J <64199550+SW0000J@users.noreply.github.com>
2020-06-09 20:05:51 +0900

    zum news 크롤링 성공!

---
+++ testcode/test2.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const url = "https://news.zum.com/issuelist/58654445"
+
+const getHtml = async () => {
+  try {
+    return await axios.get(url);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHtml()
+  .then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("div.major_news > ul").children("ul.no_reply > li.large");
+
+    $bodyList.each(function(i, elem) {
+      ulList[i] = {
+        url: 'news.zum.com' + $(this).find('div.img > a').attr('href'),
+        image_url: $(this).find('div.img > a > img').attr('src'),
+        title: $(this).find('div.txt > div.title > a').text(),
+        summary: $(this).find('div.txt > div.content > a').text(),//.slice(0, -29)
+        datetime: $(this).find('div.txt > div.content > span.etc').text()
+      };
+      //console.log(ulList[i])  // list object checking code
+    });
+
+    const data = ulList.filter(n => n.title);
+    return data;
+    //return ulList;
+  }).then(res => console.log(res));
