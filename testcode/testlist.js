f4ecff03f9254f3d200fd10f2d9b7dbce9c74d96
SW0000J <64199550+SW0000J@users.noreply.github.com>
2020-06-15 16:54:00 +0900

    testapp.js 테스트 용도

--- testcode/testlist.js
+++ testcode/testlist.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var request = require('request');
+var fs = require('fs');
+var ejs = require('ejs');
+var indexRouter = require('./routes/index');
+
+var app = express();
+const PORT = 80;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+app.listen(PORT, function(){
+    console.log('Code run in https://localhost:'+PORT);
+});
+
+
+
+
+
+
+
+
+/*
+var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
+const ejs = require('ejs');
+var engines = require('consolidate');
 const PORT = 80;
 
+// 크롤링 한 list 받아오기
 const xports = require('./test1.js');
 const zum = require('./test2.js');
 const chosun = require('./test3.js');
@@ -14,57 +49,24 @@ const yna = require('./test4.js');
 //console.log(chosun.chosun);
 //console.log(yna.yna);
 
-app.get('/', function(req, res){
-    
-    var newslist = '';
-    var newsobject = xports.xports;
-    console.log(newsobject[0].url);
-    for(var i = 0; i < newsobject.length; i++){
-        newslist = newslist +`
-        <li>
-            <div class='item'>
-                <div class='info'>
-                    <span class='time'>${newsobject[i].datetime}</span>
-                </div>
-                <div class='img'>
-                    <a>
-                        <img src = ${newsobject[i].image_url}>
-                    </a>
-                    </div>
-                <div class='news'>
-                    <a href = ${newsobject[i].url}>
-                        <strong>${newsobject[i].title}</strong>
-                    </a>
-                    <p>
-                        ${newsobject[i].summary}
-                    </p>
-                </div>
-            </div>
-        </li>`;
-    }
+// router 설정
+var indexRouter = require('./routes/index');
 
-    var html = `
-    <!DOCTYPE html>
-    <html>
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-        <head>
-            <meta charset="utf-8">
-            <title>ossswoo.tk</title>
-        </head>
+app.use(express.static('public'))
 
-        <body>
-            <div class = 'main'>
-                <div class = 'center'>
-                    <div>
-                        ${newslist}
-                    </div>
-                </div>
-            </div>
-        </body>
-    </html>`;
-    res.send(html);
-});
+app.set('news', __dirname + '/news');
+//화면 engine 설정
+app.engine('html', engines.mustache);
+app.set('news engine', 'ejs');
 
+// main 화면
+app.use('/', indexRouter);
+
+// 서버 생성
 app.listen(PORT, function(){
     console.log('Code run in https://localhost:'+PORT);
 });
+*/
